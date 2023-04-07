3de8eeb338d6775ab54ef878f682e53e7d9c48c7
Amirul Shafiq <mirullshaffiq01@gmail.com>
2023-04-07 10:01:51 +0800

    login & register apl

--- index.js
+++ index.js
@@ -41,12 +41,34 @@ app.use(express.json());
 
 app.post('/', (req, res) => {
     let data = req.body
-    res.send('Post request '
-        + JSON.stringify(data));
-})
+    res.send(
+      login(
+        data.username,
+        data.password
+      )
+    );
+});
+
+app.use(express.json());
+
+app.post('/register', (req, res) => {   
+  let data = req.body
+  res.send(
+    register(
+    data.username,
+    data.password,
+    data.name,
+    data.email
+    )
+      
+    );
+    });
+
+
+    
 
 // create a POST route for user to login
-app.post('/login', (_req, res) => {
+app.post('/login', (req, res) => {
     //get the username and password from the request bodu
     const user = dbUsers.find(user => user.username === username && user.password === password);
     
@@ -59,4 +81,32 @@ app.post('/login', (_req, res) => {
     }
 })
 
+function login(username, password){
+  console.log("someone try to login with", username, password)
+  let matched = dbUsers.find(element => 
+      element.username == username
+  )
+  if(matched) {
+      if(matched.password == password) {
+          return matched
+      } else {
+          return "Password not matched"
+      }
 
+  } else {
+      return "Username not found"
+  }
+  
+}
+
+function register(newusername, newpassword, newname, newemail){
+  //TODO: check if username exist
+
+  dbUsers.push({
+      username: newusername,
+      password: newpassword,
+      name: newname,
+      email: newemail
+  })
+return "Register Succesfull"
+}
